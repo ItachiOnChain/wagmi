5aaa45c7f6dee2b122a8c7d223c8c05f6fd4ba67
adisuyash <adityasuyashgupta@gmail.com>
2024-09-08 20:14:34 +0530

    Update forge-std submodule to commit 17656a2

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import "forge-std/Script.sol";
+import "../lib/forge-std/src/Script.sol";
 import "../src/TokenFactory.sol";
 import "../src/Token.sol";
 
 contract DeployTokenFactory is Script {
     function run() external {
-        // Start the broadcasting of transactions
-        vm.startBroadcast();
+        // Fetch private key from environment variables
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // Start broadcasting transactions with the deployer's private key
+        vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the TokenFactory contract
         TokenFactory tokenFactory = new TokenFactory();
@@ -16,7 +19,7 @@ contract DeployTokenFactory is Script {
         // Log the address of the deployed TokenFactory contract
         console.log("TokenFactory deployed at:", address(tokenFactory));
 
-        // End the broadcasting of transactions
+        // Stop broadcasting
         vm.stopBroadcast();
     }
 }
